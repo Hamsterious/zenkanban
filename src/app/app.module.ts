1ccffe6ff8837c5cc3e814ddc3961119c8f0043c
MartinJHammer <martin_hammer@live.dk>
2017-03-27 17:35:51 +0200

    Added board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ExperimentComponent } from './experiment/experiment.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExperimentComponent
+    ExperimentComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
