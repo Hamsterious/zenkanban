f312f52032dbd8eaf16d3b6dabb351804e30a423
MartinJHammer <martin_hammer@live.dk>
2017-03-27 17:41:50 +0200

    Added board class

---
+++ src/app/components/board/board.ts
@@ -0,0 +1,9 @@
+export class Board {
+    private _id : string;
+    public get id() : string {return this._id;}
+    public set id(v : string) {this._id = v;}
+
+    private _title : string;
+    public get title() : string {return this._title;}
+    public set title(v : string) {this._title = v;}
+}
