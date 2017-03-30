b1e7e70942bd5093a1bf061745374012aae24611
MartinJHammer <martin_hammer@live.dk>
2017-03-30 07:28:09 +0200

    Removed experiment, refactored app componment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ExperimentComponent } from './experiment/experiment.component';
+import { AppComponent } from './components/app/app.component';
 import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExperimentComponent,
     BoardComponent
   ],
   imports: [
