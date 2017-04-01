14aa063e6f86b25549cc4458b69b60f9e8c2a0bb
MartinJHammer <martin_hammer@live.dk>
2017-04-01 18:00:25 +0200

    Updated router module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,25 @@
+// Angular imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+// Common type imports
+import { BoardComponent } from '../components/board/board.component';
+
+// Route definitions
+const routes: Routes = [
+  { path: 'update-board/:id',  component: BoardComponent }
+];
+
+// Module meta data
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   declarations: []
 })
+
+// Exports
 export class RoutingModule { }
