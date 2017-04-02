c7631ca5832d85a26986a72c2e83b875a90bddeb
MartinJHammer <martin_hammer@live.dk>
2017-04-02 20:58:47 +0200

    Fixed path error causing app to break.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.components.scss']
 })
 
 // Exports
