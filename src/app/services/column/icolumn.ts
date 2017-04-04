feccfd27a9558d9fdf3ec756314c1026d0623ed2
martin_hammer <martin_hammer@live.dk>
2017-04-04 10:43:40 +0100

    Defined interface

--- src/app/services/column/icolumn.ts
+++ src/app/services/column/icolumn.ts
@@ -1,2 +1,10 @@
+import { Column } from "app/models/column";
+import { Observable } from "rxjs/Observable";
+
 export interface IColumn {
+    create(column: Column) : Observable<Column>;
+    getAll() : Observable<Column[]>;
+    get(id: string) : Observable<Object>;
+    update(column: Column) : Observable<Column>;
+    delete(id: string) : Observable<Column>;
 }
