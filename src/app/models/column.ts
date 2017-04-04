631ab8cccd0d3a37a6ab47817ef2148666a88074
martin_hammer <martin_hammer@live.dk>
2017-04-04 10:37:55 +0100

    Added column board

---
+++ src/app/models/column.ts
@@ -0,0 +1,7 @@
+export class Column {
+    public _id: string;
+    public title: string;
+    public order: number;
+    public boardId: string;
+    constructor(values: Object = {}) {Object.assign(this, values);}
+}
