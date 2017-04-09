a923278c1166a5481138956d8ad74e6bf21037f4
MartinJHammer <martin_hammer@live.dk>
2017-04-09 16:38:40 +0200

    - Drag and drop now works for both todos and columns. - Column drag zone needs improvement. - Position is not yet persisted. - Todos must change columnId when dragged to a new column. Not implemented yet.

--- src/app/dragula/dragula.config.ts
+++ src/app/dragula/dragula.config.ts
@@ -8,13 +8,85 @@ export class DragulaConfig {
     constructor(
         private dragulaService: DragulaService
     ) {
-         this.removeOnSpill();
+        this.disableColumnBagDragOnTodoDrag();
     }
 
-    // Methods
-    private removeOnSpill(): void {
+    // Option setter methods
+    private disableColumnBagDragOnTodoDrag(): void {
         this.dragulaService.setOptions('column-bag', {
-            removeOnSpill: true
+            moves: (el, source, handle, sibling) => {
+                // el === The column card
+                // source === The column bag
+                // handle === Clicked dom element starting the drag
+                // sibling === The element "below" the one being dragged.
+
+                // Set column-bag moves option to false if the handle contains a todo-card-part class.
+                return !handle.classList.contains('todo-card-part');;
+            }
         });
+    }
+
+    
+    // Event subscriber methods
+    private subscribeToDragEvent(): void {
+        this.dragulaService.drag.subscribe((value: any[]) => {
+            console.log(`drag: ${value[0]}`);
+            this.onDrag(value.slice(1));
+        });
+    }
+
+    private subscribeToDropEvent(): void {
+        this.dragulaService.drop.subscribe((value) => {
+            console.log(`drop: ${value[0]}`);
+            this.onDrop(value.slice(1));
+        });
+    }
+
+    private subscribeToOverEvent(): void {
+        this.dragulaService.over.subscribe((value) => {
+            console.log(`over: ${value[0]}`);
+            this.onOver(value.slice(1));
+        });
+    }
+
+    private subscribeToOutEvent(): void {
+        this.dragulaService.out.subscribe((value) => {
+            console.log(`out: ${value[0]}`);
+            this.onOut(value.slice(1));
+        });
+    }
+
+    // Activated dom element logger methods.
+    private onDrag(args: any[]) {
+        // E represents the name of the bag (class name), EL represents the actual dom element
+        let [e, el] = args;
+        console.log(e);
+        console.log(el);
+    }
+
+    private onDrop(args) {
+        let [e, el] = args;
+        console.log(e);
+        console.log(el);
+    }
+
+    private onOver(args) {
+        let [e, el, container] = args;
+        console.log("e");
+        console.log(e);
+        console.log("el");
+        console.log(el);
+        console.log("container");
+        console.log(container);
+    }
+
+    private onOut(args) {
+        let [e, el, container] = args;
+        console.log("e");
+        console.log(e);
+        console.log("el");
+        console.log(el);
+        console.log("container");
+        console.log(container);
     }
 }
